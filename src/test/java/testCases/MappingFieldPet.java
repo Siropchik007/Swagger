2eaaf227454ad21dadf972f0f56b66defc1bd9db
Siropchik007 <stas.koval.89@mail.ru>
2022-05-18 18:42:04 +0300

    создал TestBase

---
+++ src/test/java/testCases/MappingFieldPet.java
@@ -0,0 +1,32 @@
+package testCases;
+
+import help.Steps;
+import model.Pet;
+import org.junit.jupiter.api.Test;
+import utils.TestBase;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class MappingFieldPet extends TestBase {
+        Steps helper = new Steps();
+        @Test
+        public void mappingFieldPet() {
+
+            step("Создать животного");
+            Pet createPet = helper.createPet();
+
+            step("Получить информация по животному по id");
+            Pet getPet = helper.getPet(createPet.getId());
+
+            assertAll(
+                    () -> assertEquals( getPet.getName(), (createPet.getName()))
+            );
+
+        }
+
+}
+
